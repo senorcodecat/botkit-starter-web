172e84426123deb6770126d610655f6e213d60a5
Cory <senorcodecat@gmail.com>
2018-01-20 12:28:20 -0800

    Initial commit

---
+++ skills/middleware.js
@@ -0,0 +1,43 @@
+module.exports = function (controller) {
+
+    controller.middleware.ingest.use(function(bot, message, res, next) {
+
+        console.log("Ingest: ", message)
+        next()
+    
+    });
+
+	controller.middleware.receive.use(function (bot, message, next) {
+        console.log("Received a message ", message)
+        next()
+	})
+
+	controller.middleware.capture.use(function (bot, message, convo, next) {
+
+		console.log("Capture middleware running, message is ", message)
+
+        next()
+    })
+    
+    controller.middleware.send.use(function(bot, message, next) {
+
+        console.log("Send middleware, with message ", message)
+    
+        next()
+    })
+
+    controller.middleware.format.use(function(bot, message, platform_message, next) {
+    
+        console.log("Format send middleware ", message)
+        console.log("Platform message is ", platform_message)
+    
+        if (message.video) {
+            console.log("Custom field found")
+            platform_message.text = 'Replace this for custom stuff'
+            next()
+        } else {
+            next()
+        }
+    });
+
+}
